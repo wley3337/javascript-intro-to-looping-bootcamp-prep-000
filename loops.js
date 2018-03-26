f418f15fe121394f019f5d337b25e15952166360
wley3337@gmail.com <wley3337@gmail.com>
2018-03-26 13:42:18 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,20 @@
+function forLoop (array) { 
+  for (var i = 0; i < 25; i++) { 
+    if (i === 1) { array.push('I am 1 strange loop.');
+    } else {
+      array.push("I am `${i}` strange loops.");
+    };
+    return array;
+  }
+}
+
+
+//whileLoop w/ number as argument using console.log then return string 'done'. 
+
+function whileLoop(startHere) {
+  while(startHere > 0) {
+    cosole.log(--startHere);
+  }
+  return 'done';
+  
+}
